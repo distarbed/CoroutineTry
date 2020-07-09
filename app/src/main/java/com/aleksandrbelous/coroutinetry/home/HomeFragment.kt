c1d47467106f6ff9769a7bdc89617e421a70eb77
Aleksandr Belous <Belous_603>
2020-07-09 17:57:13 +0300

    Initial commit

---
+++ app/src/main/java/com/aleksandrbelous/coroutinetry/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.aleksandrbelous.coroutinetry.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * @author Aleksandr Belous
+ * @since 7/9/20.
+ */
+class HomeFragment :Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+    }
+}
